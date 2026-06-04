a01b6ed6a5e448a284c314bc0ee30acc61b6ed3f
tonett2o <tonislealt@gmail.com>
2026-06-04 18:18:51 +0200

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL =
+  typeof window !== 'undefined' && window.location.hostname === 'localhost'
+    ? 'http://localhost:8000'
+    : 'https://pluggedvercel-production.up.railway.app';
 
 export default API_URL;
